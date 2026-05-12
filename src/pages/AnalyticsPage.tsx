c554cfa1605479f02c215931ad200d47a77b44df
xavier <github@tttp.eu>
2026-05-12 22:05:50 +0200

    fix formatting

--- src/pages/AnalyticsPage.tsx
+++ src/pages/AnalyticsPage.tsx
@@ -2,11 +2,11 @@ import { AnalyticsContainer } from "@/components/analytics/AnalyticsContainer";
 import { PageLayout } from "@/components/PageLayout";
 
 export const AnalyticsPage = () => {
-	return (
-		<PageLayout>
-			<AnalyticsContainer />
-		</PageLayout>
-	);
+  return (
+    <PageLayout>
+      <AnalyticsContainer />
+    </PageLayout>
+  );
 };
 
 export default AnalyticsPage;
