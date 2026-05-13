24a1cd3e0691337f6fd109958e97d38f8c52caab
debiB <deborahberhanu644@gmail.com>
2026-05-13 17:18:01 +0300

    reply template direct commnuciation with sb

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -3,28 +3,28 @@ import { PageLayout } from "@/components/PageLayout";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { useUser } from "@/hooks/useUser"; // Import useUser hook
 export const DashboardPage = () => {
-  const { data: currentUser } = useUser();
-  if (!currentUser) return null;
-  const displayUserName = currentUser?.email
-    ? currentUser.email.split("@")[0]
-    : "Guest";
+	const { data: currentUser } = useUser();
+	if (!currentUser) return null;
+	const displayUserName = currentUser?.email
+		? currentUser.email.split("@")[0]
+		: "Guest";
 
-  return (
-    <PageLayout>
-      <Card className="p-4">
-        <CardHeader>
-          <CardTitle className="text-primary text-center">
-            Welcome to Circular Democracy!
-          </CardTitle>
-        </CardHeader>
-        <CardContent className="text-center">
-          <p className="text-lg">Hello, {displayUserName}</p>
-        </CardContent>
-      </Card>
-      {/* Add more dashboard content here */}
-      <AnalyticsContainer />
-    </PageLayout>
-  );
+	return (
+		<PageLayout>
+			<Card className="p-4">
+				<CardHeader>
+					<CardTitle className="text-primary text-center">
+						Welcome to Circular Democracy!
+					</CardTitle>
+				</CardHeader>
+				<CardContent className="text-center">
+					<p className="text-lg">Hello, {displayUserName}</p>
+				</CardContent>
+			</Card>
+			{/* Add more dashboard content here */}
+			<AnalyticsContainer />
+		</PageLayout>
+	);
 };
 
 export default DashboardPage;
